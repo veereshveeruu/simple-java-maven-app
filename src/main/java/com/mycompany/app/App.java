e13638391f1b7d37167cd1cedb25b2d3a04f050d
veereshveeruu <126373008+veereshveeruu@users.noreply.github.com>
2023-08-29 12:00:18 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello ";
+    private static final String MESSAGE = "Hello buddy ";
 
     public App() {}
 
